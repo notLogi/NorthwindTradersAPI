fcce22c5826b89aa7f0a1a6e7896cc44cd8f8071
notLogi <suroger123@gmail.com>
2025-12-09 11:25:32 -0500

    finished API exercise

--- src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/HomeController.java
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/HomeController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @RequestMapping(path = "/greetings", method = RequestMethod.GET)
-    public String index(@RequestParam(defaultValue = "World") String name) {
-        return "Hello " + name + "!";
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String index(@RequestParam(defaultValue = "World") String country) {
+        return "Hello " + country + "!";
     }
 }
